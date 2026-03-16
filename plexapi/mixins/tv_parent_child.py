fabbe18892c987816292a6cb2878fc10bf0a1977
Touchstone64 <touchstone64@gweb.me.uk>
2026-03-16 11:25:05 +0000

    Change the relational key builder to explicitly support filters in its parameters

--- plexapi/mixins/tv_parent_child.py
+++ plexapi/mixins/tv_parent_child.py
@@ -5,16 +5,20 @@ class TvParentChildMixin:
     """ Mixin for Plex objects that have parent/child relationships (episode/season/show). """
 
     def _buildRelationKey(self, key, **kwargs):
-        """ Returns a key suitable for fetching parent/child TV items """
+        """ Returns a key suitable for fetching parent/child TV items
+
+            Parameters:
+                key (str): The relational key being fetched, such as '/children' (may be
+                    empty).
+                **kwargs (dict): Custom XML attribute filters to apply to add to the
+                    query. See :func:`~plexapi.base.PlexObject.fetchItems` for more
+                    details on how this is used.
+
+        """
         if not key:
             return None
 
-        args = {}
-
-        args['includeGuids'] = int(bool(kwargs.pop('includeGuids', True)))
-        for name, value in list(kwargs.items()):
-            args[name] = value
-
+        args = {'includeGuids': 1, **kwargs}
         params = utils.joinArgs(args).lstrip('?')
 
         return f"{key}?{params}"
